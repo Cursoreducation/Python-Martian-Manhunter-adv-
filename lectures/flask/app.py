6c16e6e9e9db5cfcc3cc578a942582ce7bfddc67
serhii <serjdemidov95@gmail.com>
2021-06-15 02:08:22 +0300

    Homework 14 - Flask and Rest - done

--- lectures/flask/app.py
+++ lectures/flask/app.py
@@ -1,8 +1,11 @@
 # flask_web/app.py
+import json
+
 from flask import Flask, render_template, request, jsonify, Response
 from flask_restful import Resource, Api
 import requests
 from config import Config
+import os
 
 app = Flask(__name__)
 
@@ -41,32 +44,79 @@ class Todo(Resource):
 
     def get(self, todo_id):
         try:
-            data = {todo_id: todos[todo_id]}
+            with open('todos.json', 'r') as file:
+                all_data = json.load(file)
+                for i in all_data:
+                    if i == str(todo_id):
+                        data = {todo_id: todos[todo_id]}
+            return data
         except KeyError:
             return Response("Not found", status=404)
-        return data
+
 
     def put(self, todo_id):
-        todos[todo_id] = request.json.get('text')
-        return {todo_id: todos[todo_id]}
+        data = {todo_id: request.json.get('text')}
+        with open('todos.json', 'r') as file:
+            all_data = json.load(file)
+            for i in all_data:
+                if i == str(todo_id):
+                    all_data[i] = request.json.get('text')
+        with open('todos.json', 'w') as file:
+            json.dump(all_data, file)
+        return data
 
     def delete(self, todo_id):
-        del todos[todo_id]
+        with open(f'todos.json', 'r') as file:
+            all_data = json.load(file)
+            for i in list(all_data):
+                if i == str(todo_id):
+                    del all_data[i]
+        with open(f'todos.json', 'w') as file:
+            json.dump(all_data, file)
         return Response(todos, status=204)
 
 
 class TodoList(Resource):
 
     def get(self):
+        with open('todos.json', 'w') as file:
+            json.dump(todos, file)
         return todos
 
     def post(self):
         todos[request.json.get('todo_id', None)] = request.json.get('text', "")
+        with open('todos.json', 'w') as file:
+            json.dump(todos, file)
         return todos
+
+
+class WeatherAPI(Resource):
+
+    def get(self):
+        weather = []
+        city_name = request.args.get('city')
+        cities_list = city_name.split(",")
+        for city in cities_list:
+            querystring = {"q": city, "cnt": 1, "mode": "null", "lon": '', "type": "link, accurate", "lat": "",
+                           "units": "imperial, metric"}
+
+            headers = {
+                'x-rapidapi-key': Config.WEATHER_API_KEY,
+                'x-rapidapi-host': Config.WEATHER_API_HOST
+            }
+
+            response = requests.request("GET", Config.WEATHER_API_URL, headers=headers, params=querystring)
+            if response.status_code == 200:
+                data = response.json()
+                weather.append(data['list'])
+            else:
+                return Response(status=404)
+        return weather
 
 
 api.add_resource(Todo, '/todos/<int:todo_id>')
 api.add_resource(TodoList, '/todos')
+api.add_resource(WeatherAPI, '/weather')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- lectures/flask/tests/test_api.py
+++ lectures/flask/tests/test_api.py
@@ -1,5 +1,6 @@
 from tests.conftest import client, todos
 import json
+from config import Config
 
 
 def test_create(client, todos):
@@ -34,3 +35,18 @@ def test_delete(client):
     assert response.status_code == 204
     response = client.get("/todos/1")
     assert response.status_code == 404
+
+
+def test_weather(client):
+    Config.WEATHER_API_KEY = "8fd89dd0famsh2e179986560c1c9p1dfcedjsnd32b7762c9bc"
+    Config.WEATHER_API_URL = "https://community-open-weather-map.p.rapidapi.com/find"
+    Config.WEATHER_API_HOST = "community-open-weather-map.p.rapidapi.com"
+    response = client.get("/weather?city=london")
+    assert response.status_code == 200
+    assert response.json[0][0]['name'] == 'London'
+    response2 = client.get("/weather?city=london,kyiv")
+    assert response2.status_code == 200
+    assert response2.json[0][0]['name'] == 'London'
+    assert response2.json[1][0]['name'] == 'Kyiv'
+    response3 = client.get("/weather?city=L")
+    assert response3.status_code == 404
