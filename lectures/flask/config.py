8ab8e8394c1e141bd6953a445a9effdaa00cf391
serhii <serjdemidov95@gmail.com>
2021-06-30 18:30:07 +0300

    Homework 19 - Lecture

--- lectures/flask/config.py
+++ lectures/flask/config.py
@@ -11,7 +11,7 @@ class Config:
     MENU_ITEMS = [
         {
             'name': "Articles",
-            'link': '/articles',
+            'link': '/article/create',
         },
         {
             'name': "Categories",
